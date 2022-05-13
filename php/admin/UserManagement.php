9d5cdc8ecd98b70f1f5ff3a586c37f911ca9da44
Augusto Britez <agussb52@gmail.com>
2022-05-13 19:51:24 -0300

    Added navBar to template, and user Table

--- php/admin/UserManagement.php
+++ php/admin/UserManagement.php
@@ -1,8 +1,8 @@
 <?php
     session_start();  
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-        //If it wasn´t, we render the login forms
-        include "../templates/Login.html";
+        //If the user is already logged, we show the page
+        include "../../templates/UserManagement.html";
     } else {
         //If the user isn´t autenticated, we send him to the login page
         header("Location: php/Login.php");
